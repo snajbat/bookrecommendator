1bcd4900f736160d718ca59c6a12d3600553aa8a
Sandra <snajera@ucm.es>
2021-10-26 23:19:38 +0200

    making the project beautiful

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,11 +1,21 @@
 import streamlit as st
 from utils.recommendation import recommend
 
-st.title("Book recommendation system")
-st.subheader("Select your book and enjoy!")
+col1, col2 = st.columns([6,1])
+with col1:
+    st.title("Book recommendation system")
+with col2:
+    st.image("https://p.kindpng.com/picc/s/293-2934582_from-the-blog-logo-book-black-png-transparent.png", width=100)
+
+st.markdown("<h4 style='text-align: center;'>Select your book and enjoy!</h4>", unsafe_allow_html=True)
+
 
 st.text("")
-book = st.text_input("Enter the name of a book you like")
+st.text("")
+st.text("")
+st.text("")
+st.text("")
+book = st.text_input("Enter the title of a book you like with the first letter of each word capitalized and press Enter.")
 
 
 if book:
